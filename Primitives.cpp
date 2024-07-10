1ace3dd409dae4043747fb6180b9be8ce7f107d6
Peter Otrebus-Larsson <otrebus@gmail.com>
2024-07-10 22:40:29 +0200

    wip

--- Primitives.cpp
+++ Primitives.cpp
@@ -17,13 +17,13 @@ std::pair<std::vector<Vertex3>, std::vector<int>> buildCylinder(real length, rea
 
 	for(int i = 0; i < N; i++)
 	{
-		auto u = y*std::cos(i*2*pi/N)*radius + z*std::sin(i*2*pi/N)*radius - x*length/2;
+		auto u = y*std::cos(i*2*pi/N)*radius + z*std::sin(i*2*pi/N)*radius;
 		points.push_back(u);
 	}
 
 	for(int i = 0; i < N; i++)
 	{
-		auto v = y*std::cos(i*2*pi/N)*radius + z*std::sin(i*2*pi/N)*radius + x*length/2;
+		auto v = y*std::cos(i*2*pi/N)*radius + z*std::sin(i*2*pi/N)*radius + x;
 		points.push_back(v);
 	}
 
@@ -89,6 +89,71 @@ Model3d* createCylinderModel(real length, real radius, int N)
     auto material = new LambertianMaterial({ 0, 0.8, 0.1 });
 
 	auto [vertices, triangles] = buildCylinder(length, radius, N);
+
+	auto model = new Model3d();
+    Mesh3d* mesh = new Mesh3d(vertices, triangles, material);
+    model->addMesh(*mesh);
+
+	return model;
+}
+
+std::pair<std::vector<Vertex3>, std::vector<int>> buildCone(real length, real radius, int N)
+{
+	Vector3 x = { 1, 0, 0 }, y = { 0, 1, 0 }, z = { 0, 0, 1 };
+
+	std::vector<Vector3> points;
+	std::vector<Vertex3> vertices;
+	std::vector<int> indices;
+
+	for(int i = 0; i < N; i++)
+	{
+		auto u = y*std::cos(i*2*pi/N)*radius + z*std::sin(i*2*pi/N)*radius;
+		points.push_back(u);
+	}
+	points.push_back( { length, 0, 0 } );
+
+	for(int i = 0; i < N; i++)
+	{
+		auto u = points[i];
+
+		Vector3 a = points[i] - points[N];
+		Vector3 v = points[i];
+		auto n_u = (a%v)%a;
+
+		Vertex3 U(u, n_u, { 0, 0 });
+		vertices.push_back(U);
+	}
+
+	for(int i = 0; i < N; i++)
+	{
+		auto u = points[N+i];
+		Vector3 n_u = { 0, points[N+i].y, points[N+i].z };
+		Vertex3 U(u, n_u, { 0, 0 });
+		vertices.push_back(U);
+	}
+
+	for(int i = 0; i < N; i++)
+	{
+		indices.push_back(i);
+		indices.push_back((i+1)%N);
+		indices.push_back(N+i);
+	}
+
+	for(int i = 0; i < N; i++)
+	{
+		indices.push_back(N);
+		indices.push_back(N+i);
+		indices.push_back(N+(i+1)%N);
+	}
+
+	return { vertices, indices };
+}
+
+Model3d* createConeModel(real length, real radius, int N)
+{
+    auto material = new LambertianMaterial({ 0, 0.8, 0.1 });
+
+	auto [vertices, triangles] = buildCone(length, radius, N);
 
 	auto model = new Model3d();
     Mesh3d* mesh = new Mesh3d(vertices, triangles, material);
