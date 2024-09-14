9d8123b32e209129386f29f52f957a73dfe93df2
Otrebus <otrebus@gmail.com>
2024-09-14 18:48:57 +0200

    fix sdf

--- DebugDraw.cpp
+++ DebugDraw.cpp
@@ -344,26 +344,27 @@ int drawTexts(GLFWwindow* window, int xres, int yres)
 
 std::vector<real> calcSigned(std::vector<unsigned char> v, int xres, int yres)
 {
-    struct vecr { real x, y; bool operator<(const vecr& v) const { return std::make_pair(v.x, v.y) > std::make_pair(x, y); } };
-    struct vec { int x, y; bool operator<(const vec& v) const { return std::make_pair(v.x, v.y) > std::make_pair(x, y); } };
-    std::vector<bool> data(xres*yres);
-    std::vector<real> cost(xres*yres);
-    std::vector<vecr> vecs(xres*yres);
+    struct vecr { real x, y; bool operator<(const vecr& v) const { return std::make_pair(x, y) < std::make_pair(v.x, v.y); } };
+    struct vec { int x, y; bool operator<(const vec& v) const { return std::make_pair(x, y) < std::make_pair(v.x, v.y); } };
+    std::vector<bool> data(xres * yres);
+    std::vector<real> cost(xres * yres, inf);
+    std::vector<vecr> vecs(xres * yres);
+    std::vector<char> vis(xres * yres, false);
 
     for(int y = 0; y < yres; y++)
     {
         for(int x = 0; x < xres; x++)
         {
-            data[y*xres+x] = v[y*xres*3+x*3];
-            cost[y*xres+x] = data[y*xres+x] ? -inf : inf;
+            data[y * xres + x] = v[y * xres * 3 + x * 3];
         }
     }
 
-    auto get = [xres, &data] (int x, int y) { return data[y*xres+x]; };
-    auto getCost = [xres, &cost] (int x, int y) -> real& { return cost[y*xres+x]; };
-    auto getVec = [xres, &vecs] (int x, int y) -> vecr& { return vecs[y*xres+x]; };
-    auto within = [xres, yres] (int x, int y) { return x >= 0 && y >= 0 && x < xres && y < yres; };
-    auto dist = [] (real x, real y) { return std::sqrt(x*x + y*y); };
+    auto get = [xres, &data](int x, int y) { return data[y * xres + x]; };
+    auto getVis = [xres, &vis](int x, int y) -> char& { return vis[y * xres + x]; };
+    auto getCost = [xres, &cost](int x, int y) -> real& { return cost[y * xres + x]; };
+    auto getVec = [xres, &vecs](int x, int y) -> vecr& { return vecs[y * xres + x]; };
+    auto within = [xres, yres](int x, int y) { return x >= 0 && y >= 0 && x < xres && y < yres; };
+    auto dist = [](real x, real y) { return std::sqrt(x * x + y * y); };
 
     std::set<std::pair<real, vec>> q;
 
@@ -398,14 +399,74 @@ std::vector<real> calcSigned(std::vector<unsigned char> v, int xres, int yres)
         auto c = it->first;
         auto p = it->second;
         q.erase(it);
-        if(c > getCost(p.x, p.y))
+
+        
+        if(getVis(p.x, p.y))
             continue;
+        getVis(p.x, p.y) = true;
 
         for(int dx = -1; dx <= 1; dx++)
         {
             for(int dy = -1; dy <= 1; dy++)
             {
-                if((dx || dy) && within(p.x+dx, p.y+dy) && !get(p.x+dx, p.y+dy))
+                if(within(p.x+dx, p.y+dy) && !getVis(p.x+dx, p.y+dy) && !get(p.x+dx, p.y+dy))
+                {
+                    auto v = getVec(p.x, p.y);
+                    vecr v2 { v.x+dx, v.y+dy };
+                    if(auto cost = dist(v2.x, v2.y); cost < getCost(p.x+dx, p.y+dy))
+                    {
+                        getCost(p.x+dx, p.y+dy) = cost;
+                        getVec(p.x+dx, p.y+dy) = v2;
+                        q.insert(std::make_pair(cost, vec { p.x+dx, p.y+dy }));
+                    }
+                }
+            }
+        }
+    }
+
+    std::fill(vis.begin(), vis.end(), false);
+
+    for(int y = 0; y < yres; y++)
+    {
+        for(int x = 0; x < xres; x++)
+        {
+            if(get(x, y))
+                continue;
+
+            for(int dx = -1; dx <= 1; dx++)
+            {
+                for(int dy = -1; dy <= 1; dy++)
+                {
+                    if((dx || dy) && within(x+dx, y+dy) && get(x+dx, y+dy))
+                    {
+                        auto c = dist(dx, dy)/2;
+                        if(getCost(x+dx, y+dy) > c)
+                        {
+                            q.insert({ c, { x+dx, y+dy } });
+                            getVec(x+dx, y+dy) = { real(dx)/2, real(dy)/2 };
+                            getCost(x+dx, y+dy) = c;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    while(!q.empty())
+    {
+        auto it = q.begin();
+        auto c = it->first;
+        auto p = it->second;
+        q.erase(it);
+        
+        if(getVis(p.x, p.y))
+            continue;
+        getVis(p.x, p.y) = true;
+
+        for(int dx = -1; dx <= 1; dx++)
+        {
+            for(int dy = -1; dy <= 1; dy++)
+            {
+                if(within(p.x+dx, p.y+dy) && !getVis(p.x+dx, p.y+dy) && get(p.x+dx, p.y+dy))
                 {
                     auto v = getVec(p.x, p.y);
                     vecr v2 { v.x+dx, v.y+dy };
@@ -424,52 +485,8 @@ std::vector<real> calcSigned(std::vector<unsigned char> v, int xres, int yres)
     {
         for(int x = 0; x < xres; x++)
         {
-            if(get(x, y))
-                continue;
-
-            for(int dx = -1; dx <= 1; dx++)
-            {
-                for(int dy = -1; dy <= 1; dy++)
-                {
-                    if((dx || dy) && within(x+dx, y+dy) && get(x+dx, y+dy))
-                    {
-                        auto c = -dist(dx, dy)/2;
-                        if(getCost(x+dx, y+dy) < c)
-                        {
-                            q.insert({ c, { x+dx, y+dy } });
-                            getVec(x+dx, y+dy) = { real(dx)/2, real(dy)/2 };
-                            getCost(x+dx, y+dy) = c;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    while(!q.empty())
-    {
-        auto it = q.begin();
-        auto c = it->first;
-        auto p = it->second;
-        q.erase(it);
-        if(c < getCost(p.x, p.y))
-            continue;
-
-        for(int dx = -1; dx <= 1; dx++)
-        {
-            for(int dy = -1; dy <= 1; dy++)
-            {
-                if((dx || dy) && within(p.x+dx, p.y+dy) && get(p.x+dx, p.y+dy))
-                {
-                    auto v = getVec(p.x, p.y);
-                    vecr v2 { v.x+dx, v.y+dy };
-                    if(auto cost = -dist(v2.x, v2.y); cost > getCost(p.x+dx, p.y+dy))
-                    {
-                        getCost(p.x+dx, p.y+dy) = cost;
-                        getVec(p.x+dx, p.y+dy) = v2;
-                        q.insert(std::make_pair(cost, vec { p.x+dx, p.y+dy }));
-                    }
-                }
-            }
+            if(getVis(x, y))
+                getCost(x, y) = - getCost(x, y);
         }
     }
 
@@ -484,9 +501,9 @@ std::vector<real> calcSigned(std::vector<unsigned char> v, int xres, int yres)
     for(auto& c : cost)
     {
         if(c < 0)
-            c = (25.f + std::max(c, -25.f))/50.f;
+            c = (50.f + std::max(c, -50.f))/100.f;
         else
-            c = (25.f + std::min(25.f, c))/50.f;
+            c = (50.f + std::min(50.f, c))/100.f;
     }
 
     return cost;
@@ -511,10 +528,10 @@ int drawSigned(GLFWwindow* window, int xres, int yres)
     auto [data, width, height] = readBMP("signsource.bmp");
 
     std::vector<Vertex3> vs = {
-        {{-0.15, -0.15, 0.0}, {0.0, 0.0, 1.0}, {0, 0}},
-        {{ 0.15, -0.15, 0.0}, {0.0, 0.0, 1.0}, {1, 0}},
-        {{ 0.15,  0.15, 0.0}, {0.0, 0.0, 1.0}, {1, 1}},
-        {{-0.15,  0.15, 0.0}, {0.0, 0.0, 1.0}, {0, 1}}
+        {{-0.55, -0.55, 0.0}, {0.0, 0.0, 1.0}, {0, 0}},
+        {{ 0.55, -0.55, 0.0}, {0.0, 0.0, 1.0}, {1, 0}},
+        {{ 0.55,  0.55, 0.0}, {0.0, 0.0, 1.0}, {1, 1}},
+        {{-0.55,  0.55, 0.0}, {0.0, 0.0, 1.0}, {0, 1}}
     };
 
     std::vector<int> indices = { 0, 1, 2, 2, 3, 0 };
